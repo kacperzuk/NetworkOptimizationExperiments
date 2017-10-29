d7d3e86403a3492bdd7856674e3163ae43a449ac
Kacper Żuk <kacper.b.zuk@gmail.com>
2017-10-29 23:28:19 +0100

    Bugfix

--- greedy_resolver.py
+++ greedy_resolver.py
@@ -60,32 +60,18 @@ if __name__ == "__main__":
             p = nx.dijkstra_path(tmpgraph, d[0], d[1])
         except nx.exception.NetworkXNoPath:
             minedge = None
-            for v in tmpgraph.nodes:
-                if (v,d[0]) in graph.edges:
-                    continue
-                tmpgraph2 = tmpgraph.copy()
-                tmpgraph2.add_edge(d[0], v)
-                tmpgraph2.add_edge(v, d[0])
-                cost = link_cost(tmpgraph2.nodes[v], tmpgraph2.nodes[d[0]])
-                if not minedge or cost < minedge[2]:
-                    try:
-                        p = nx.dijkstra_path(tmpgraph2, d[0], d[1])
-                    except nx.exception.NetworkXNoPath:
-                        continue
-                    minedge = (v, d[0], cost, p)
-            for v in tmpgraph.nodes:
-                if (v,d[1]) in graph.edges:
-                    continue
-                tmpgraph2 = tmpgraph.copy()
-                tmpgraph2.add_edge(d[1], v)
-                tmpgraph2.add_edge(v, d[1])
-                cost = link_cost(tmpgraph2.nodes[v], tmpgraph2.nodes[d[1]])
-                if not minedge or cost < minedge[2]:
-                    try:
-                        p = nx.dijkstra_path(tmpgraph2, d[0], d[1])
-                    except nx.exception.NetworkXNoPath:
-                        continue
-                    minedge = (v, d[1], cost, p)
+            for vt1 in tmpgraph.nodes:
+                for vt2 in tmpgraph.nodes:
+                    tmpgraph2 = tmpgraph.copy()
+                    tmpgraph2.add_edge(vt1, vt2)
+                    tmpgraph2.add_edge(vt2, vt1)
+                    cost = link_cost(tmpgraph2.nodes[vt2], tmpgraph2.nodes[vt2])
+                    if not minedge or cost < minedge[2]:
+                        try:
+                            p = nx.dijkstra_path(tmpgraph2, d[0], d[1])
+                        except nx.exception.NetworkXNoPath:
+                            continue
+                        minedge = (vt1, vt2, cost, p)
             if not minedge:
                 print("Couldn't find path for demand:", d)
                 raise Exception("Unsolvable with greedy algorithm")
