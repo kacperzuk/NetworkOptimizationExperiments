a0825d0c5e79a464876c310eb21cac74127eec63
Kacper Żuk <kacper.b.zuk@gmail.com>
2017-11-07 17:00:31 +0100

    Add support for backup paths

--- greedy_resolver.py
+++ greedy_resolver.py
@@ -43,12 +43,20 @@ def solve(network_file, link_capacity):
     return graph, demands
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: greedy_resolver.py network_file.xml link_capacity")
+    if len(sys.argv) < 3 or len(sys.argv) % 2 != 1:
+        print("Usage: greedy_resolver.py network_file.xml link_capacity premium_pairs...")
         print("  - link_capacity is in Gbit/s")
         sys.exit(2)
     graph, demands = solve(sys.argv[1], int(sys.argv[2]))
+
+    premiums = sys.argv[3:]
+    premium_pairs = []
+
+    while premiums:
+        premium_pairs.append((premiums.pop(),premiums.pop()))
+
     demands.sort(key=lambda x: x[2], reverse=True)
+
     tunnels = []
     for d in demands:
         tmpgraph = graph.copy()
@@ -81,9 +89,46 @@ if __name__ == "__main__":
             graph.add_edge(minedge[0], minedge[1], capacity_left=int(sys.argv[2]), cost=minedge[2]/2)
             graph.add_edge(minedge[1], minedge[0], capacity_left=int(sys.argv[2]), cost=minedge[2]/2)
 
-        tunnels.append(p)
+        is_premium = (d[0], d[1]) in premium_pairs or (d[1], d[0]) in premium_pairs
+        tunnels.append({ "path": p, "premium": is_premium, "backup": False })
         for e in nx.utils.pairwise(p):
             graph.edges[e]["capacity_left"] -= d[2]
+
+        if is_premium:
+            tmpgraph = graph.copy()
+
+            edges_to_remove = list(nx.utils.pairwise(p))
+            for v1, v2, attr in tmpgraph.edges(data=True):
+                if attr["capacity_left"] < d[2]:
+                    edges_to_remove.append((v1,v2))
+            tmpgraph.remove_edges_from(edges_to_remove)
+
+            try:
+                p = nx.dijkstra_path(tmpgraph, d[0], d[1])
+            except nx.exception.NetworkXNoPath:
+                minedge = None
+                for vt1 in tmpgraph.nodes:
+                    for vt2 in tmpgraph.nodes:
+                        tmpgraph2 = tmpgraph.copy()
+                        tmpgraph2.add_edge(vt1, vt2)
+                        tmpgraph2.add_edge(vt2, vt1)
+                        cost = link_cost(tmpgraph2.nodes[vt2], tmpgraph2.nodes[vt2])
+                        if not minedge or cost < minedge[2]:
+                            try:
+                                p = nx.dijkstra_path(tmpgraph2, d[0], d[1])
+                            except nx.exception.NetworkXNoPath:
+                                continue
+                            minedge = (vt1, vt2, cost, p)
+                if not minedge:
+                    print("Couldn't find path for demand:", d)
+                    raise Exception("Unsolvable with greedy algorithm")
+                graph.add_edge(minedge[0], minedge[1], capacity_left=int(sys.argv[2]), cost=minedge[2]/2)
+                graph.add_edge(minedge[1], minedge[0], capacity_left=int(sys.argv[2]), cost=minedge[2]/2)
+
+            tunnels.append({ "path": p, "premium": True, "backup": True })
+            for e in nx.utils.pairwise(p):
+                graph.edges[e]["capacity_left"] -= d[2]
+
 
     print("Tunnels")
     pprint(tunnels)
